344e9211761b003f859cca16950e5257e048e755
DaravathSaiteja <102550946+saiteja-18@users.noreply.github.com>
2023-02-21 17:12:47 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,5 @@
 provider "aws" {
   region = "eu-west-2"
-  shared_config_files = ["/home/daravathsaiteja3/.aws/config"]
-  shared_credentials_files = ["/home/daravathsaiteja3/.aws/credentials"]
 }
 
 resource "aws_instance" "example" {
