a0152119cc341ce903c7dd5627a34701e5b3ab54
Daravath saiteja <daravathsaiteja3@gmail.com>
2023-02-21 19:23:56 +0000

    adding file

--- main.tf
+++ main.tf
@@ -4,14 +4,14 @@ provider "aws" {
 
 resource "aws_key_pair" "my_key" {
   key_name   = "my_key"
-  public_key = file("~/.ssh/my_key.pub")
+  # public_key = file("~/.ssh/my_key.pub")
 }
 
 resource "aws_instance" "example" {
   ami           = "ami-09ee0944866c73f62"  # The ID of the Amazon Machine Image (AMI) to use
   instance_type = "t2.micro"              # The instance type
   subnet_id     = "subnet-03a399803c7c6794d"  # The ID of the subnet to launch the instance in
-  key_name = aws_key_pair.my_key.k
+  key_name = aws_key_pair.my_key.key_name
  
   user_data = <<-EOF
               #!/bin/bash
