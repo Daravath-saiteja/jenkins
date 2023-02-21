de9a11da46d18f425ba8e578735cef7ccfb802bf
Daravath saiteja <daravathsaiteja3@gmail.com>
2023-02-21 20:24:45 +0000

    adding file

--- main.tf
+++ main.tf
@@ -16,13 +16,6 @@ resource "aws_key_pair" "example" {
 #   # public_key = file("~/.ssh/my_key.pub")
 #   public_key = file("$/home/daravathsaiteja3/.ssh/my_key.pub")
 # }
-
-resource "aws_instance" "example" {
-  ami           = "ami-09ee0944866c73f62"  # The ID of the Amazon Machine Image (AMI) to use
-  instance_type = "t2.micro"              # The instance type
-  subnet_id     = "subnet-03a399803c7c6794d"  # The ID of the subnet to launch the instance in
-  key_name = aws_key_pair.example.key_name
-
 resource "aws_security_group" "ssh-access" {
   name_prefix = "ssh-access-"
 
@@ -33,6 +26,14 @@ resource "aws_security_group" "ssh-access" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_instance" "example" {
+  ami           = "ami-09ee0944866c73f62"  # The ID of the Amazon Machine Image (AMI) to use
+  instance_type = "t2.micro"              # The instance type
+  subnet_id     = "subnet-03a399803c7c6794d"  # The ID of the subnet to launch the instance in
+  key_name = aws_key_pair.example.key_name
+  security_groups = [aws_security_group.ssh-access.id]
+
  
   user_data = <<-EOF
               #!/bin/bash
