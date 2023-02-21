65246f0f90d064e366e4a20860551642134689db
Daravath saiteja <daravathsaiteja3@gmail.com>
2023-02-21 19:42:19 +0000

    adding file

--- main.tf
+++ main.tf
@@ -4,7 +4,8 @@ provider "aws" {
 
 resource "aws_key_pair" "my_key" {
   key_name   = "my_key"
-  public_key = file("~/.ssh/my_key.pub")
+  # public_key = file("~/.ssh/my_key.pub")
+  public_key = file("${path.module}/my_key.pub")
 }
 
 resource "aws_instance" "example" {
